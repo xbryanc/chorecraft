1943f192242aa5834450310647385542fa85bad0
Bryan Chen <bchen21@mit.edu>
2019-11-16 13:03:20 -0500

    quest model

---
+++ server/models/Quest.js
@@ -0,0 +1,14 @@
+// import node modules
+const mongoose = require('mongoose');
+
+// define schema
+const QuestModelSchema = new mongoose.Schema ({
+    title: String,
+    description: String,
+    exp: Number,
+    coins: Number,
+    parentId: mongoose.Schema.Types.ObjectId,
+    childrenId: [mongoose.Schema.Types.ObjectId],
+});
+
+module.exports = mongoose.model('QuestModel', QuestModelSchema);
